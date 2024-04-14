60742594e95bc38a9baaed004aa69458b5457f7e
Diego Gutierrez vazquez <dieguga04@gmail.com>
2024-04-14 20:29:52 +0200

    prueba

--- src/modules/InitialPart/pages/Register.vue
+++ src/modules/InitialPart/pages/Register.vue
@@ -15,7 +15,39 @@ export default{
 
 <template>
 
-<p>Register</p>
+<main>
+
+<section class="login_imgpart">
+
+    <p class="login_imgpart__title">WELCOME BACK!</p>
+
+    <div class="login_imgpart__registerPart">
+
+        <p>YOU DONT HAVE ACCOUNT ?</p>
+        <button @click="goRegister">SIGN UP</button>
+
+    </div>
+
+
+</section>
+
+
+<section class="login">
+
+    <p>LOGIN</p>
+
+    <form class="login_form">
+
+        <input type="text" placeholder="User">
+        <input type="text" placeholder="Passwoord">
+
+        <button>ENTER</button>
+
+    </form>
+
+</section>
+
+</main>
 
 </template>
 
