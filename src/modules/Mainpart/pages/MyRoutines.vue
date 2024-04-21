922b6566c427895d8db1290b29107e023e1ed587
Diego Gutierrez vazquez <dieguga04@gmail.com>
2024-04-21 22:17:48 +0200

    myroutines update

--- src/modules/Mainpart/pages/MyRoutines.vue
+++ src/modules/Mainpart/pages/MyRoutines.vue
@@ -1,31 +1,98 @@
 <script>
 import UserNavbar from '../components/UserNavbar.vue';
-export default{
+import Footer from '../../InitialPart/components/Footer.vue';
 
-    components:{UserNavbar}
+export default {
+  components: { UserNavbar, Footer },
 
+  data() {
+    return {
+     
+    }
+  },
+ 
+  methods: {
+ 
+  }
 
-
-
-
-
+  
 }
 
 </script>
 
-
 <template>
+    <main>
+      <UserNavbar></UserNavbar>
+  
+      <section>
 
-<UserNavbar></UserNavbar>
+        <article class="routine_container">
+            
+          <h1>YOUR ROUTINES</h1>
+  
+          <article>
+            <h3>Lunes</h3>
+  
+            <div class="routine_container__routine">
+           
+              <div class="routine_container__exercise">
+                <p>ejercicio 1</p>
+                <img src="" alt="imagen">
+                <p>series</p>
+                <p>repetitions</p>
+              </div>
+
+              <div class="routine_container__exercise">
+                <p>ejercicio 1</p>
+                <img src="" alt="imagen">
+                <p>series</p>
+                <p>repetitions</p>
+              </div>
+
+              <div class="routine_container__buttons">
+                <button>editar</button>
+                <button>eliminar</button>
+              </div>
+            </div>
+          </article>
+
+        </article>
+  
+        <article class="current_routine_container">
+          
+        </article>
+
+      </section>
+
+    </main>
+
+    <Footer></Footer>
 
 </template>
+  
 
-
-<style scoped>
-
-
-
-
-
-
-</style>
+  
+  <style scoped>
+  
+  .routine_container__routine {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    border: 1px solid black;
+    min-width: 300px;
+    max-width: 90%;
+    gap: 4px;
+  }
+  
+  .routine_container__buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+  
+  .routine_container__buttons button {
+    margin-left: 5px;
+  }
+  
+  </style>
+  
